10391964ce5114ec0f22e4c1c7e9b5daade21cd0
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-07-27 14:17:28 -0300

    feat: creates post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from './components/Post'
 
 import styles from './App.module.css'
 
 import './global.css';
-import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
@@ -11,9 +12,7 @@ function App() {
       <Header/>
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>
-          <p>post</p>
-        </main>
+        <Post />
       </div>
     </>
   )
