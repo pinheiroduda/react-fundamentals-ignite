cd0f465f9582b621a28e9dfc40e973f0ea90bf97
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-07-28 09:33:53 -0300

    feat: creates Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
@@ -7,7 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/pinheiroduda.png" />
+          <Avatar src="https://github.com/pinheiroduda.png"/>
           <div className={styles.authorInfo}>
             <strong>Eduarda Pinheiro</strong>
             <span>Intern Software Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css'
 
@@ -10,7 +11,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1586996292898-71f4036c4e07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50" 
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/pinheiroduda.png" />
+        <Avatar src="https://github.com/pinheiroduda.png"/>
 
         <strong>Eduarda Pinheiro</strong>
         <span>Intern Software Developer</span>
