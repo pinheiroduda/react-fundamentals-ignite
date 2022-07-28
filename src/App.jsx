2327cc2f2e5f0807780a51c73b94fb17e69c524c
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-07-28 10:39:39 -0300

    feat: adds iteration to app posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,48 @@ import styles from './App.module.css'
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/pinheiroduda.png",
+      name: "Eduarda Pinheiro",
+      role: "Intern Software Developer at CI&T"
+    },
+    content: [
+      {type: "paragraph", content: "Fala galeraa 👋" },
+      {type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      {type: "link", content: "jane.design/doctorcare"}
+    ],
+    publishedAt: new Date('2022-07-28 11:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/pinheiroduda.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat"
+    },
+    content: [
+      {type: "paragraph", content: "Fala galeraa 👋" },
+      {type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+      {type: "link", content: "jane.design/doctorcare"}
+    ],
+    publishedAt: new Date('2022-07-29 10:00:00'),
+  }
+]
+
 function App() {
   return (
     <>
       <Header/>
       <div className={styles.wrapper}>
         <Sidebar />
-        <Post />
+        <main>
+          {posts.map(post => {
+            return <Post />
+          })}
+        </main>
       </div>
     </>
   )
