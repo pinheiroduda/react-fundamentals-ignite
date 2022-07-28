ec2eed4354c1a199014d1cc782693515653a40b9
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-07-28 15:00:24 -0300

    feat: adds disabled and required propertie to 'publicar' button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,7 +33,12 @@ export function Post({ author, content, publishedAt }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório.');
   }
 
   function deleteComment(commentToDelete) {
@@ -43,6 +48,8 @@ export function Post({ author, content, publishedAt }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -83,10 +90,12 @@ export function Post({ author, content, publishedAt }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
         </footer>
       </form>
 
